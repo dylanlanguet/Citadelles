1b7f80ac1ffcdf86fbbcb2dd2013be6c783c7bfa
Dylan LANGUET <dylan.languet@viacesi.fr>
2025-03-06 14:45:12 +0100

    Commit

--- app/context/gameContext.js
+++ app/context/gameContext.js
@@ -4,13 +4,13 @@ import { createContext, useContext, useState } from 'react';
 
 const GameContext = createContext();
 
-export const GameProvider = ({ children }) => {
+export const GameProvider = ({ children, players }) => {
   const [gameConfig, setGameConfig] = useState({
     numberOfPlayers: 4,
-    players: [],
+    players: players,
     // Vous pouvez ajouter d'autres propriétés ici (deck, tour, etc.)
   });
-
+  console.log(players);
   const updateGameConfig = (newConfig) => {
     setGameConfig(newConfig);
   };
