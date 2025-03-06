fe3666247ad32f9e76a52c8c5bddf3499e7193ff
Bottero Romain <you@example.com>
2025-03-06 14:55:12 +0100

    P

--- models/GameEngine.js
+++ models/GameEngine.js
@@ -1,14 +1,26 @@
 export class GameEngine {
   constructor(players) {
-    this.players = players; // tableau d'instances de Player
+    this.players = players; // Tableau d'instances de Player
     this.currentTurn = 1;   // Numéro du tour courant
-    this.currentPlayerIndex = 0; // Index du joueur dont c'est le tour
+    this.currentPlayerIndex = 0; // Index du joueur actif
+    this.phase = 'characterSelection'; // Phase initiale
   }
 
-  // Passe au tour suivant : incrémente le numéro de tour et sélectionne le joueur suivant
+  // Passe au tour suivant dans la phase d'action
   nextTurn() {
     this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
     this.currentTurn++;
+  }
+
+  // Permet de changer de phase : de la sélection de personnages à la phase d'action
+  nextPhase() {
+    if (this.phase === 'characterSelection') {
+      // Ici, vous pouvez, par exemple, vérifier que chaque joueur a choisi un personnage
+      // et éventuellement réorganiser l'ordre de passage selon un critère (par ex. par ordre de valeur du personnage)
+      this.phase = 'action';
+      this.currentTurn = 1;
+      this.currentPlayerIndex = 0; // ou un nouvel ordre basé sur la sélection
+    }
   }
 
   // Retourne le joueur courant
